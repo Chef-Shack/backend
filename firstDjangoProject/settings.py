1730fc79d9addf796d598452f5a031101e4926d6
ZB0SS <zbk0786@gamil.com>
2022-10-24 20:14:00 -0400

    Add CORS

--- firstDjangoProject/settings.py
+++ firstDjangoProject/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'members',
     'recipes',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
@@ -50,6 +51,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware'
+]
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000'
 ]
 
 ROOT_URLCONF = 'firstDjangoProject.urls'
